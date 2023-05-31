c52c8eb4e41745399c13bcb911461abe421cb69f
juliana <ceriap860@gmail.com>
2023-05-31 14:26:31 +0700

    octoriana

--- app/src/main/java/com/octoriana201103650/hotelapp/MainActivity.java
+++ app/src/main/java/com/octoriana201103650/hotelapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
